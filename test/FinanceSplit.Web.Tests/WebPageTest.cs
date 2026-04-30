482ff80f5fbff0b5f5e2f158bca14f1283dab7af
Alan Hoffmann <alanzhoffmann@gmail.com>
2026-04-30 22:10:41 +0000

    fix(test): use real Kestrel server for Playwright E2E tests
    TestWebApplicationFactory uses TestServer which isn't accessible via TCP.
    Playwright needs an actual HTTP server. Spin up a real WebApplication
    with Kestrel on a random port (http://127.0.0.1:0) instead.

--- test/FinanceSplit.Web.Tests/WebPageTest.cs
+++ test/FinanceSplit.Web.Tests/WebPageTest.cs
@@ -1,8 +1,12 @@
+using FinanceSplit.Application;
 using FinanceSplit.Data;
+using FinanceSplit.Data.Interfaces;
+using FinanceSplit.Data.Repositories;
+using FinanceSplit.Domain.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Playwright;
-using TUnit.AspNetCore;
 using TUnit.Core;
 using TUnit.Playwright;
 
@@ -11,27 +15,36 @@ namespace FinanceSplit.Web.Tests;
 [Category("E2E")]
 public abstract class WebPageTest : PageTest, IAsyncDisposable
 {
-    protected WebTestFactory Factory { get; private set; } = null!;
     protected string BaseUrl { get; private set; } = null!;
+    private WebApplication _app = null!;
 
     [Before(HookType.Test)]
     public async Task SetupWebApp()
     {
         var dbName = $"WebTest_{Guid.NewGuid()}";
-        Factory = new WebTestFactory();
 
-        Factory.WithWebHostBuilder(builder =>
-        {
-            builder.ConfigureServices(services =>
-            {
-                services.AddDbContext<FinanceSplitDbContext>(options => options.UseInMemoryDatabase(dbName));
-            });
-        });
+        var builder = WebApplication.CreateBuilder(
+            new WebApplicationOptions { ApplicationName = typeof(Program).Assembly.GetName().Name!, EnvironmentName = "Development" }
+        );
 
-        var client = Factory.CreateClient();
-        BaseUrl = client.BaseAddress!.ToString().TrimEnd('/');
+        builder.Services.AddDbContext<FinanceSplitDbContext>(options => options.UseInMemoryDatabase(dbName));
+        builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+        builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+        builder.Services.AddApplicationServices();
+        builder.Services.AddSingleton<IMigrationState>(new TestMigrationState());
+        builder.Services.AddRazorComponents();
 
-        using var scope = Factory.Services.CreateScope();
+        _app = builder.Build();
+        _app.Urls.Add("http://127.0.0.1:0");
+
+        _app.UseAntiforgery();
+        _app.MapStaticAssets();
+        _app.MapRazorComponents<FinanceSplit.Web.Components.App>();
+
+        await _app.StartAsync();
+        BaseUrl = _app.Urls.First();
+
+        using var scope = _app.Services.CreateScope();
         var db = scope.ServiceProvider.GetRequiredService<FinanceSplitDbContext>();
         await db.Database.EnsureCreatedAsync();
     }
@@ -43,7 +56,12 @@ public abstract class WebPageTest : PageTest, IAsyncDisposable
 
     public async ValueTask DisposeAsync()
     {
-        await Factory.DisposeAsync();
+        await _app.DisposeAsync();
         GC.SuppressFinalize(this);
+    }
+
+    private class TestMigrationState : IMigrationState
+    {
+        public bool IsDone { get; set; } = true;
     }
 }
