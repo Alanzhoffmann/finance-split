aadbdd7ecda77b7655873bfa553c784a4bd5490c
Alan Hoffmann <alanzhoffmann@gmail.com>
2026-05-01 13:53:35 +0000

    feat(ui): add MudBlazor 9 and setup layout with navigation drawer

--- src/FinanceSplit.Web/Program.cs
+++ src/FinanceSplit.Web/Program.cs
@@ -1,8 +1,10 @@
 using FinanceSplit.Web.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ApiClient>();
 
